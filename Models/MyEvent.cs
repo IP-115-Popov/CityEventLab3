80bf796af079942b4fd2217e8518dae03162a0f8
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-03-19 19:52:29 +0700

    MyEvent

---
+++ Models/MyEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityEventLab3.Models
+{
+    internal class MyEvent
+    {
+        public MyEvent() { }
+        public string ImagePath { get; set; }
+        public string Header { get; set; }
+        public string Description { get; set; }       
+        public string Date { get; set; }
+        public string Price { get; set; }
+
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,6 @@ namespace CityEventLab3.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+
     }
 }
